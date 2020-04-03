a22ee852a3eb7bbfecb4ab49da71faccb79d073f
rauhaan-khan <rauhaank07@gmail.com>
2020-04-03 13:09:07 +0500

    Data import completed

--- RegressionModel.py
+++ RegressionModel.py
@@ -1,1 +1,12 @@
-print("Hello World")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+# import statsmodels.api as sm
+
+data = pd.read_csv("data/Advertising.csv")
+
+print(data.head())
+
