fdae7f7b3fdb35805f1e62b3af85a6a78cdf7703
rauhaan-khan <rauhaank07@gmail.com>
2020-04-03 13:32:24 +0500

    Linear Regression performed and statistics displayed.

--- RegressionModel.py
+++ RegressionModel.py
@@ -1,12 +1,25 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
 
-# import statsmodels.api as sm
+import statsmodels.api as sm
 
 data = pd.read_csv("data/Advertising.csv")
 
-print(data.head())
+Xs = data.drop(['sales', 'Unnamed: 0'], axis=1)
+y = data['sales'].values.reshape(-1, 1)
 
+reg = LinearRegression()
+reg.fit(Xs, y)
+print("The linear model is: Y = {:.5} + {:.5}*TV + "
+      "{:.5}*radio + {:.5}*newspaper".format(reg.intercept_[0],
+                                             reg.coef_[0][0],
+                                             reg.coef_[0][1],
+                                             reg.coef_[0][2]))
+
+X = np.column_stack((data['TV'], data['radio'], data['newspaper']))
+y = data['sales']
+X2 = sm.add_constant(X)
+est = sm.OLS(y, X2)
+est2 = est.fit()
+print(est2.summary())
